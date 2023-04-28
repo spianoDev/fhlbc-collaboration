4c19e80dfd81a3fdd320c10505f2b39a913e6a70
jerrvon <jerrvonewing.cs@gmail.com>
2023-04-28 10:48:02 -0500

    Added comments to code

--- fruit.py
+++ fruit.py
@@ -1,13 +1,18 @@
+# Initialized list of fruits
 fruits = ["Apple", "Mango", "Banana", "Peach"]
 
+# Print each fruit using for loop 
 for fruit in fruits:
     print(fruit)
 print("\n")
 
+# Print each fruit using for loop and length of list
 for fruit in range(0, len(fruits)):
     print(fruits[fruit])
 print("\n")
 
+
+# Print each fruit in the list using the length of list and while loop
 i = 0
 while(i < len(fruits)):
     print(fruits[i])
