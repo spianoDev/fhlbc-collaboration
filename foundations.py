ada27ee80e03505e1d9e2ea0eb1be95dca14d0da
Sarah Panaligan <spianodev@gmail.com>
2023-06-09 08:15:52 -0500

    set up for review of foundational python concepts

---
+++ foundations.py
@@ -0,0 +1,20 @@
+## FOUNDATIONAL CONCEPTS FOR PYTHON ##
+
+# Variables #
+my_var =
+
+## PRO TIP - avoid using full names of identified variable types like string, list, dictionary, and number ##
+
+# If/Else Statements #
+
+# Lists #
+my_lst = []
+
+# Looping through Lists #
+
+# Dictionaries #
+my_dict = {
+
+}
+
+# Looping through Dictionaries #
